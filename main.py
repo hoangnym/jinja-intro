064efd58db980006d354bd23d04ca00d918aa02b
hoangnym <hoangnym94@gmail.com>
2021-07-19 20:24:08 +0200

    finish up posts page

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from flask import Flask, render_template
 import requests
 
+all_posts = requests.get("https://api.npoint.io/706ae5b51a7218849686").json()
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    all_posts = requests.get("https://api.npoint.io/706ae5b51a7218849686").json()
-    print(all_posts)
     return render_template(
         "index.html",
         blog=all_posts
     )
 
-@app.route('/blog/<num>')
+
+@app.route('/post/<num>')
 def get_blog(num):
     return render_template(
         "post.html",
-        num=num
+        num=num,
+        blog=all_posts[int(num)]
     )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
