15968da4feddc9d5d975d8143e8f103ff6418c0f
hoangnym <hoangnym94@gmail.com>
2021-07-19 14:24:55 +0200

    use agify and genderize api for guessing game

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import random
 import datetime as dt
+import requests
 
 app = Flask(__name__)
 
@@ -13,6 +14,18 @@ def home():
         num=random_number,
         current_year=dt.datetime.now().year,
         author="Hoang"
+    )
+
+
+@app.route('/guess/<name>')
+def guess(name):
+    gender = requests.get(f"https://api.genderize.io/?name={name}").json()["gender"]
+    age = requests.get(f"https://api.agify.io/?name={name}").json()["age"]
+    return render_template(
+        "guess.html",
+        name=name.capitalize(),
+        gender=gender,
+        age=age
     )
 
 
