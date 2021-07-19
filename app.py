6083080c20484ce033ea3fed3878d7a19537c07e
hoangnym <hoangnym94@gmail.com>
2021-07-19 14:13:42 +0200

    add footer dynamically

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
+import datetime as dt
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'Hello World!'
+    random_number = random.randint(1, 10)
+    return render_template(
+        "index.html",
+        num=random_number,
+        current_year=dt.datetime.now().year,
+        author="Hoang"
+    )
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
